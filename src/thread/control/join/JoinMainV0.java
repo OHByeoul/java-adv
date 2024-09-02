98784e8e60c863ea5df75e65894a98611b92782d
오한별 <suriessay@naver.com>
2024-09-02 16:19:21 +0900

    feat : 스레드 join

---
+++ src/thread/control/join/JoinMainV0.java
@@ -0,0 +1,12 @@
+package thread.control.join;
+
+import static util.MyLogger.log;
+
+public class JoinMainV0 {
+    public static void main(String[] args) {
+        log("Start");
+        log("End");
+    }
+
+
+}
